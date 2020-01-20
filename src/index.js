5eb48f21c325d55c6830ab17286eadad43201105
Igor Alisson <igoralissoncn@gmail.com>
2020-01-20 15:23:05 -0300

    criacao de metodo put e delete ok

--- src/index.js
+++ src/index.js
@@ -14,12 +14,48 @@ const projects = [];
 
 app.post("/projects", (req, res) => {
   const { id, title } = req.body;
-  projects.push({ id, title, tasks: [] });
-  return res.json(projects);
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+  return res.json(project);
 });
 
 app.get("/projects", (req, res) => {
   return res.json(projects);
+});
+
+app.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+  project.title = title;
+
+  return res.json(project);
+});
+
+app.delete("projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const indexProject = projects.findIndex(p => p.id == id);
+  projects.slice(indexProject, 1);
+
+  return res.send("deleted");
+});
+
+app.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+
+  const project = projects.find(p => p.id == id);
+  project.tasks.push(task);
+
+  return res.json(project);
 });
 
 app.listen(3000);
